8a78b75d539c13f029bd698c5b77f5ac0b35c39d
rahulsantha2006 <rahul.santha.2006@gmail.com>
2020-03-25 18:26:49 -0500

    Challenge 3 complete

---
+++ narwhals-challenge/src/main/java/com/rahul/challenge3/MultiplyTwoNumbers_Program3.java
@@ -0,0 +1,12 @@
+package com.rahul.challenge3;
+
+public class MultiplyTwoNumbers_Program3 {
+
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y = 5;
+
+        System.out.println(x*y);
+    }
+}
