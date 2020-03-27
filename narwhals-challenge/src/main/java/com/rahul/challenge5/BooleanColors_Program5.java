711f5551a0c3e80cba0014049f1ebbed702836d1
rahulsantha2006 <rahul.santha.2006@gmail.com>
2020-03-27 18:24:18 -0500

    Challenge 5 complete

---
+++ narwhals-challenge/src/main/java/com/rahul/challenge5/BooleanColors_Program5.java
@@ -0,0 +1,18 @@
+package com.rahul.challenge5;
+
+public class BooleanColors_Program5 {
+
+    public static void main(String[] args) {
+
+        boolean colors;
+
+        colors = true;
+
+        if (colors == true){
+            System.out.println("Your Color is Green");
+        }
+        else {
+            System.out.println("Your Color is Red");
+        }
+    }
+}
