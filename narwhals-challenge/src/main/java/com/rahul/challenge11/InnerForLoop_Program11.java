0163528a05eb8ed6726c12a88de89b44beeb9669
rahulsantha2006 <rahul.santha.2006@gmail.com>
2020-04-02 12:59:15 -0500

    Challenge 11 complete

---
+++ narwhals-challenge/src/main/java/com/rahul/challenge11/InnerForLoop_Program11.java
@@ -0,0 +1,19 @@
+package com.rahul.challenge11;
+
+public class InnerForLoop_Program11 {
+
+    public static void main(String[] args) {
+
+
+        for (int a=1; a<=5; a++){
+            System.out.println("Regular For Loop");
+
+            for (int b=a; b<=4; b++){
+                System.out.println("Inner For Loop");
+            }
+        }
+
+
+
+    }
+}
