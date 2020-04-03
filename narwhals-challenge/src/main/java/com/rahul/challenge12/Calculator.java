2133b615a95185dbfaa020049453bd18090bbc0b
rahulsantha2006 <rahul.santha.2006@gmail.com>
2020-04-03 13:20:38 -0500

    Challenge 12 complete

---
+++ narwhals-challenge/src/main/java/com/rahul/challenge12/Calculator.java
@@ -0,0 +1,10 @@
+package com.rahul.challenge12;
+
+public class Calculator {
+
+    public void Food (){
+
+        System.out.println(" My favorite food is chicken biriyani");
+    }
+
+}
---
+++ narwhals-challenge/src/main/java/com/rahul/challenge12/Program12example.java
@@ -0,0 +1,11 @@
+package com.rahul.challenge12;
+
+public class Program12example {
+
+    public static void main(String[] args) {
+
+        Calculator obj = new Calculator();
+        obj.Food();
+
+    }
+}
