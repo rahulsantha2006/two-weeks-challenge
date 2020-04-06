474c544e9abc98ead52b43d533b58160306cbb5c
rahulsantha2006 <rahul.santha.2006@gmail.com>
2020-04-05 20:25:46 -0500

    Challenge 14 complete

---
+++ narwhals-challenge/src/main/java/com/rahul/challenge14/Animals.java
@@ -0,0 +1,15 @@
+package com.rahul.challenge14;
+
+public class Animals {
+
+    public Animals(){
+
+        System.out.println("Cat");
+    }
+
+    public Animals( String name){
+
+        System.out.println(name);
+    }
+
+}
---
+++ narwhals-challenge/src/main/java/com/rahul/challenge14/Program14Example.java
@@ -0,0 +1,10 @@
+package com.rahul.challenge14;
+
+public class Program14Example {
+
+    public static void main(String[] args) {
+
+        Animals obj = new Animals();
+        Animals obj2 = new Animals("Dog");
+    }
+}
