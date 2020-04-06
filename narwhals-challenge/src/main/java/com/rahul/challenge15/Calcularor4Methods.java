67243a224545a2c8dffef4e376425141c118ef7e
rahulsantha2006 <rahul.santha.2006@gmail.com>
2020-04-06 13:56:20 -0500

    Challenge 15 complete

---
+++ narwhals-challenge/src/main/java/com/rahul/challenge15/Calcularor4Methods.java
@@ -0,0 +1,25 @@
+package com.rahul.challenge15;
+
+public class Calcularor4Methods {
+
+    public void add (double a, double b){
+        System.out.println(a+b);
+    }
+
+
+    public void subtract(double a, double b){
+        System.out.println(a-b);
+    }
+
+
+    public void multiply(double a, double b){
+        System.out.println(a*b);
+    }
+
+
+    public void divide(double a, double b){
+        System.out.println(a/b);
+    }
+
+
+}
---
+++ narwhals-challenge/src/main/java/com/rahul/challenge15/Program15Example.java
@@ -0,0 +1,14 @@
+package com.rahul.challenge15;
+
+public class Program15Example {
+
+    public static void main(String[] args) {
+
+        Calcularor4Methods obj = new Calcularor4Methods();
+
+        obj.add(2,1);
+        obj.subtract(2,1);
+        obj.multiply(2,1);
+        obj.divide(2,1);
+    }
+}
